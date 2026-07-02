2cf86c0be0eb17b512feedaacf3412217f990d94
Craig Duncan <craig.duncan@estrato.co.uk>
2026-07-02 10:34:25 +0100

    Remove the hacky reflection workaround

--- crontab/expression_test.go
+++ crontab/expression_test.go
@@ -1,32 +1,19 @@
 package crontab
 
 import (
-	"errors"
-	"reflect"
 	"testing"
-	"unsafe"
+	"time"
 
 	"github.com/estratocloud/go-crontab/crontab/internal"
+
+	"github.com/estratocloud/go-crontab/crontab/day"
+	"github.com/estratocloud/go-crontab/crontab/hour"
+	"github.com/estratocloud/go-crontab/crontab/minute"
+	"github.com/estratocloud/go-crontab/crontab/month"
+	"github.com/estratocloud/go-crontab/crontab/weekday"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func getField(value string, err error) internal.Field {
-	field := internal.Field{}
-	v := reflect.ValueOf(&field).Elem()
-
-	valuef := v.FieldByName("value")
-	valueptr := unsafe.Pointer(valuef.UnsafeAddr())
-	reflect.NewAt(valuef.Type(), valueptr).Elem().Set(reflect.ValueOf(value))
-
-	if err != nil {
-		errf := v.FieldByName("err")
-		errptr := unsafe.Pointer(errf.UnsafeAddr())
-		reflect.NewAt(errf.Type(), errptr).Elem().Set(reflect.ValueOf(err))
-	}
-
-	return field
-}
 
 func Test_NewExpression1(t *testing.T) {
 	want := &Expression{
@@ -54,14 +41,14 @@ func Test_NewExpression2(t *testing.T) {
 
 func Test_NewExpression3(t *testing.T) {
 	want := &Expression{
-		Minute:  getField("0", nil),
+		Minute:  minute.Minute(0),
 		Hour:    internal.Every(),
 		Day:     internal.Every(),
 		Month:   internal.Every(),
 		Weekday: internal.Every(),
 	}
 	got := NewExpression(&Expression{
-		Minute: getField("0", nil),
+		Minute: minute.Minute(0),
 	})
 	assert.Equal(t, want, got)
 }
@@ -69,13 +56,13 @@ func Test_NewExpression3(t *testing.T) {
 func Test_NewExpression4(t *testing.T) {
 	want := &Expression{
 		Minute:  internal.Every(),
-		Hour:    getField("0", nil),
+		Hour:    hour.Hour(0),
 		Day:     internal.Every(),
 		Month:   internal.Every(),
 		Weekday: internal.Every(),
 	}
 	got := NewExpression(&Expression{
-		Hour: getField("0", nil),
+		Hour: hour.Hour(0),
 	})
 	assert.Equal(t, want, got)
 }
@@ -84,12 +71,12 @@ func Test_NewExpression5(t *testing.T) {
 	want := &Expression{
 		Minute:  internal.Every(),
 		Hour:    internal.Every(),
-		Day:     getField("1", nil),
+		Day:     day.Day(1),
 		Month:   internal.Every(),
 		Weekday: internal.Every(),
 	}
 	got := NewExpression(&Expression{
-		Day: getField("1", nil),
+		Day: day.Day(1),
 	})
 	assert.Equal(t, want, got)
 }
@@ -99,11 +86,11 @@ func Test_NewExpression6(t *testing.T) {
 		Minute:  internal.Every(),
 		Hour:    internal.Every(),
 		Day:     internal.Every(),
-		Month:   getField("1", nil),
+		Month:   month.Month(time.January),
 		Weekday: internal.Every(),
 	}
 	got := NewExpression(&Expression{
-		Month: getField("1", nil),
+		Month: month.Month(time.January),
 	})
 	assert.Equal(t, want, got)
 }
@@ -114,10 +101,10 @@ func Test_NewExpression7(t *testing.T) {
 		Hour:    internal.Every(),
 		Day:     internal.Every(),
 		Month:   internal.Every(),
-		Weekday: getField("1", nil),
+		Weekday: weekday.Weekday(time.Sunday),
 	}
 	got := NewExpression(&Expression{
-		Weekday: getField("1", nil),
+		Weekday: weekday.Weekday(time.Sunday),
 	})
 	assert.Equal(t, want, got)
 }
@@ -138,48 +125,38 @@ func Test_Format2(t *testing.T) {
 }
 
 func Test_Format3(t *testing.T) {
-	want := errors.New("bad minute")
-	field := getField("", want)
-
+	field := minute.Minute(333)
 	got, err := NewExpression(&Expression{Minute: field}).Format()
 	assert.Equal(t, "", got)
-	assert.ErrorIs(t, err, want)
+	assert.Errorf(t, err, "invalid minute (333) must be between 0 and 59")
 }
 
 func Test_Format4(t *testing.T) {
-	want := errors.New("bad hour")
-	field := getField("", want)
-
+	field := hour.Hour(40)
 	got, err := NewExpression(&Expression{Hour: field}).Format()
 	assert.Equal(t, "", got)
-	assert.ErrorIs(t, err, want)
+	assert.Errorf(t, err, "invalid hour (40) must be between 0 and 23")
 }
 
 func Test_Format5(t *testing.T) {
-	want := errors.New("bad day")
-	field := getField("", want)
-
+	field := day.Day(50)
 	got, err := NewExpression(&Expression{Day: field}).Format()
 	assert.Equal(t, "", got)
-	assert.ErrorIs(t, err, want)
+	assert.Errorf(t, err, "invalid day (50) must be between 0 and 31")
 }
 
 func Test_Format6(t *testing.T) {
-	want := errors.New("bad month")
-	field := getField("", want)
-
+	field := month.Month(13)
 	got, err := NewExpression(&Expression{Month: field}).Format()
 	assert.Equal(t, "", got)
-	assert.ErrorIs(t, err, want)
+	assert.Errorf(t, err, "invalid month (13) must be between 1 and 12")
 }
 
 func Test_Format7(t *testing.T) {
-	want := errors.New("bad weekday")
-	field := getField("", want)
-
+	field := weekday.Weekday(7)
 	got, err := NewExpression(&Expression{Weekday: field}).Format()
 	assert.Equal(t, "", got)
-	assert.ErrorIs(t, err, want)
+	assert.Errorf(t, err, "invalid weekday (7) must be between 0 and 6")
 }
 
 func Test_MustFormat1(t *testing.T) {
@@ -196,12 +173,11 @@ func Test_MustFormat2(t *testing.T) {
 }
 
 func Test_MustFormat3(t *testing.T) {
-	want := errors.New("bad minute")
-	field := getField("", want)
+	field := minute.Minute(300)
 
 	e := NewExpression(&Expression{Minute: field})
 
-	assert.PanicsWithErrorf(t, "unhandled error, use Format() to handle: bad minute", func() {
+	assert.PanicsWithErrorf(t, "unhandled error, use Format() to handle: invalid minute (300) must be between 0 and 59", func() {
 		_ = e.MustFormat()
 	}, "MustFormat() should panic")
 }
