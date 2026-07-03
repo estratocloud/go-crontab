94cb991f9bf61ef0a85c3f70eae5da8e0c213aa7
Craig Duncan <craig.duncan@estrato.co.uk>
2026-07-03 08:32:49 +0100

    Create the core structures and formatting logic

---
+++ crontab/internal/field.go
@@ -0,0 +1,35 @@
+package internal
+
+import (
+	"fmt"
+)
+
+type Field struct {
+	value string
+	err   error
+}
+
+// Error Check if there were any invalid values (eg hour=35)
+func (f Field) Error() error {
+	return f.err
+}
+
+// Format Get the field's string value (eg "*/5")
+// or an error for invalid values (eg hour=35)
+func (f Field) Format() (string, error) {
+	return f.value, f.err
+}
+
+// MustFormat Similar to Format() but will panic if any invalid values were provided,
+// ONLY use this method with known safe values, if you need to generate expressions
+// from user input then use Format() and handle errors properly.
+func (f Field) MustFormat() string {
+	if f.err != nil {
+		panic(fmt.Errorf("unhandled error, use Format() to handle: %w", f.err))
+	}
+	return f.value
+}
+
+func Every() Field {
+	return Field{value: "*"}
+}
