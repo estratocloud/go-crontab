37737257bc04fb9b69b39538887aa7aa7b5082a0
Craig Duncan <craig.duncan@estrato.co.uk>
2026-07-06 10:22:14 +0100

    Create field validation helper methods (#3)

--- crontab/internal/field.go
+++ crontab/internal/field.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 )
 
 type Field struct {
@@ -30,7 +32,73 @@ func (f Field) MustFormat() string {
 	return f.value
 }
 
+func validate(name string, min int, max int, values ...int) error {
+	if len(values) == 0 {
+		return fmt.Errorf("no %s values were passed", name)
+	}
+	for _, value := range values {
+		if value < min || value > max {
+			return fmt.Errorf("invalid %s (%d) must be between %d and %d", name, value, min, max)
+		}
+	}
+	return nil
+}
+
 // Every Create a Field instance that represents every possible value ("*")
 func Every() Field {
 	return Field{value: "*"}
+}
+
+// Every Create a Field instance for a single value ("0")
+func Value(name string, min int, max int, value int) Field {
+	err := validate(name, min, max, value)
+	if err != nil {
+		return Field{err: err}
+	}
+	return Field{value: strconv.Itoa(value)}
+}
+
+// List Create a Field instance for a list of values ("4,8,12")
+func List(name string, min int, max int, values ...int) Field {
+	err := validate(name, min, max, values...)
+	if err != nil {
+		return Field{err: err}
+	}
+
+	values = append([]int(nil), values...)
+	sort.Ints(values)
+
+	result := ""
+	for _, value := range values {
+		if result != "" {
+			result += ","
+		}
+		result += strconv.Itoa(value)
+	}
+
+	return Field{value: result}
+}
+
+// Range Create a Field instance for a range of values ("3-5")
+func Range(name string, min int, max int, from int, to int) Field {
+	err := validate(name, min, max, from, to)
+	if err != nil {
+		return Field{err: err}
+	}
+	if from > to {
+		return Field{err: fmt.Errorf("invalid %s range, 'from' (%d) must be less than 'to' (%d)", name, from, to)}
+	}
+	return Field{value: fmt.Sprintf("%d-%d", from, to)}
+}
+
+// Range Create a Field instance for a step of values ("*/5")
+func EveryX(name string, max int, value int) Field {
+	if value == 1 {
+		return Field{value: "*"}
+	}
+	err := validate(name, 1, max, value)
+	if err != nil {
+		return Field{err: err}
+	}
+	return Field{value: fmt.Sprintf("*/%d", value)}
 }
